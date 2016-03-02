754583f5c9c1f54fa17d2114c03d62cf312bd290
Ilmir Taychinov <riml@railgun-games.com>
2016-03-02 01:07:32 -0500

    last class update: mix+frequency

---
+++ Assets/Scripts/getMicAudio.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//set dependency (i.e. this needs an audio source) 
+[RequireComponent(typeof(AudioSource))]
+
+public class getMicAudio : MonoBehaviour {
+
+	public AudioSource audio;
+	public int audioSamplesToTake = 64;
+	public int audioSampleChannels = 0; //1=mono, 2=stereo, etc.
+	private string audioOutDevice;
+	private int minAudioFreq;
+	private int maxAudioFreq;
+	private float recTimer = 0;
+	private float recRefreshTime = 10;
+	public float audioListenerSensitivity;
+	public float audioVolume;
+	private bool isThereDevice = false;
+	
+	// Use this for initialization
+	void Start () {
+		//getting audio source
+		audio = GetComponent<AudioSource>();
+	
+		//getting audio device
+		try{
+			audioOutDevice = Microphone.devices[0].ToString(); //array used in case there is more than one device
+			Microphone.GetDeviceCaps(audioOutDevice, out minAudioFreq, out maxAudioFreq);
+			Debug.Log(audioOutDevice);
+		}
+		catch 
+		{
+			Debug.Log("No audio device");
+		}
+
+		if (audioOutDevice != null){
+			isThereDevice = true;
+		}
+
+		//start recording
+		//null returns default device
+		audio.clip = Microphone.Start(audioOutDevice, true, 10, 44100);
+	
+		audio.loop = true; //reusing audio clip, setting it to loop
+		audio.mute = true; //mute to avoid overlapping audio
+		while (!(Microphone.GetPosition(audioOutDevice) > 0)) {} //wait for sound (gets sample position of mic)
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//make sure there is input 
+		if (isThereDevice == true) {
+
+			//timer until renewing the recording
+			recTimer += Time.deltaTime;
+
+			if (recTimer >= recRefreshTime) {
+				StopRecording ();
+				StartRecording ();
+				recTimer = 0;
+			}
+		}
+
+		//for simplicity's sake, this allows for tweaking of input sound
+		audioVolume = NormalizeAudio () * audioListenerSensitivity;
+	}
+
+	//Begin mic recording into audio clip, replacing old clip
+	void StartRecording(){
+		audio.clip = Microphone.Start(audioOutDevice, true, 10, 44100);
+		while (!(Microphone.GetPosition(audioOutDevice) > 0)) {} //wait for sound
+		audio.Play();
+	}
+
+	//Stop mic recording (to allow for new audio clip)
+	void StopRecording(){
+		audio.Stop ();
+		Microphone.End (audioOutDevice);
+	}
+
+	float NormalizeAudio(){
+
+		//take and store samples
+		float[] outputData = new float[audioSamplesToTake];
+		float s = 0;
+		audio.GetOutputData (outputData, audioSampleChannels);
+
+		//finding the average of the samples
+		foreach (float i  in outputData) {
+			s += Mathf.Abs (i);
+		}
+
+		return s / audioSamplesToTake;
+	}
+}
--- Assets/Scripts/lineVisualizer.cs
+++ Assets/Scripts/lineVisualizer.cs
@@ -1,29 +1,57 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;  
+using System.Collections;  
 
-public class lineVisualizer : MonoBehaviour {
+public class LineVisualizer : MonoBehaviour  
+{  
+	public float xOffset;										// Offset for the visualization along the x axis
+	private AudioSource aSource;  								// Audio Source
+	private float[] samples = new float[256];  					// Sample Rate
+	private LineRenderer lRenderer;  							// Line renderer to draw the information
+	private Transform audioVisTransform;						// Transform of audio visualizer
+	public GameObject cube;  									// Prefab to determine line height, mesh will not be rendered
+	private Vector3 cubePos;  									// Temp cube position to draw the line points with
+	private Transform[] cubesTransform;  						// Store the Transforms of all instantiated cubes   
+	private Vector3 gravity = new Vector3(0.0f,0.25f,0.0f);  	// The velocity that the cubes will drop 
 
-	public int detail; // Level of detail of the audio information
-	public float amplitude; // Audio level
-	private float startPosition; // Start position of the visualizer
+	void Awake ()  
+	{  
+		this.audioVisTransform = GetComponent<Transform> ();
+		this.aSource = GetComponent<AudioSource>();  
+		this.lRenderer = GetComponent<LineRenderer>();  
+	}  
 
-	// Use this for initialization
-	void Start () {
-		detail = 500;
-		amplitude = 0.1f;
-		startPosition = transform.localPosition.y;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float[] info = new float[detail]; // intialize the array to hold a certain amount of information i.e. detail
-		AudioListener.GetOutputData (info, 0); // populate the array with data from the audio listener
-		float packagedData = 0.0f;
+	void Start()  
+	{  
+		lRenderer.SetVertexCount(samples.Length); 				// Set the number of vertexes for the line based on sample rate
+		cubesTransform = new Transform[samples.Length];  		// Set the number of cube transforms based on sample rate
+		GameObject tempCube;  									// Temp cube to be accessed as most recent clone
 
-		for (int x = 0; x < info.Length; x++) {
-			packagedData += System.Math.Abs(info[x]); // Assign the value of the audio output data to a value
-		}
+		// For each sample instatiate each 'cube' that will determine line height
+		for(int i = 0; i < samples.Length; i++)  
+		{  
+			tempCube = (GameObject) Instantiate(cube, new Vector3(audioVisTransform.position.x + i, audioVisTransform.position.y, audioVisTransform.position.z),Quaternion.identity);  
+			cubesTransform[i] = tempCube.GetComponent<Transform>();  
+			cubesTransform[i].parent = audioVisTransform;  
+		}  
+	}  
 
-		transform.position = new Vector3(0, startPosition + packagedData * amplitude, 0); // Moves the line visualizer along
-	}
-}
+	void Update ()  
+	{  
+		aSource.GetSpectrumData(this.samples, 0, FFTWindow.BlackmanHarris);  // Grab the samples from the frequency bands
+
+		//For each sample  
+		for(int i = 0; i < samples.Length; i++)  
+		{  
+			// Set cube position
+			cubePos.Set(cubesTransform[i].position.x, Mathf.Clamp(samples[i]*(300 + i*i), 0, 300), cubesTransform[i].position.z);  
+
+			// If the new position is greater set it, otherwise let it fall
+			if(cubePos.y >= cubesTransform[i].position.y)  
+				cubesTransform[i].position = cubePos;  
+			else  
+				cubesTransform[i].position -= gravity;  
+				 
+			lRenderer.SetPosition(i, cubePos - new Vector3(xOffset, 0, 0)); // Draw the line
+		}  
+	}  
+}  
